5772c82a18766f66fdad4ad1c2cb66446af31188
huurray <huurray@gmail.com>
2019-01-11 23:38:56 +0900

    add page

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
-
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('currentYear', () => {
@@ -41,6 +37,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
